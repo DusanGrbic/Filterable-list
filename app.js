256bbe72f07aad700ab8e58d66f271cdb884c969
Dusan Grbic <dusangrbic984@gmail.com>
2020-03-09 14:33:11 +0100

    Added hiding headers

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 (function () {
-  const filter = document.getElementById('filter');
-  const names = document.querySelectorAll('.collection-item a');
+  const filter   = document.getElementById('filter');
+  const names    = document.querySelectorAll('.collection-item a');
+  const wrappers = document.querySelectorAll('.wrapper');
+
+  let counter;
 
 
   const filterNames = () => {
@@ -13,6 +16,28 @@
         name.textContent.toLowerCase().indexOf(input) === -1
           ? 'none'
           : 'block';
+    });
+
+    // Manage display of headers
+    headerDisplay();
+  }
+
+  const headerDisplay = () => {
+    // For each wrapper div
+    wrappers.forEach(wrapper => {
+      // Initially set header display to block
+      wrapper.children[0].style.display = 'block';
+
+      counter = 0;
+
+      // Cycle through wrappers children and if child's display is 'none', increase counter
+      Array.from(wrapper.children).forEach(child => {
+        if (child.style.display === 'none') counter++;
+      });
+
+      // If display of all of the wrappers children is set to 'none' (except for the header),
+      // hide the header
+      if (counter === wrapper.children.length - 1) wrapper.children[0].style.display = 'none';
     });
   }
 
